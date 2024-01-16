1703c8b822e8517444f21333aaa38fa21d8960da
Lenesky <Lenesky@mails.com>
2024-01-16 23:43:11 +0000

    New Data

--- ReadData.py
+++ ReadData.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import pickle
 
-PairsPrice = np.array(pd.read_csv('PriceData_Trading.csv', usecols = ["BAC", "JPM"]))
+PairsPrice = pd.read_csv('PriceData_Trading.csv', usecols = ["BAC", "JPM"])
 
-with open('PairsPrice.pkl', 'wb') as f:
-    pickle.dump(PairsPrice, f)
+print(PairsPrice["BAC"][0:10])
