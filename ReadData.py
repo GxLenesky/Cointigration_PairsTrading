8398a2546f9b305194ff1151d81d3d9a9e025a82
Lenesky <Lenesky@mails.com>
2024-01-20 17:15:56 +0000

    Finished the initial code and tested on real data

--- ReadData.py
+++ ReadData.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import pickle
 
-price_trading = pd.read_csv('PriceData_Trading.csv', index_col = "Date")
-price_training = pd.read_csv('PriceData_Train.csv', index_col = "Date")
-price_full = pd.read_csv('PriceData_Full.csv', index_col = "Date")
-price_trainexcl = pd.read_csv('PriceData_TrainExcl2020.csv', index_col = "Date")
+price_trading = pd.read_csv('PriceData_Trading.csv')
+price_training = pd.read_csv('PriceData_Train.csv')
+price_full = pd.read_csv('PriceData_Full.csv')
+price_trainexcl = pd.read_csv('PriceData_TrainExcl2020.csv')
 
 """with open('PriceData_Trading.pkl', 'wb') as f:
     pickle.dump(price_trading, f)
@@ -16,4 +16,5 @@ with open('PriceData_Full.pkl', 'wb') as f:
 with open('PriceData_TrainExcl2020.pkl', 'wb') as f:
     pickle.dump(price_trainexcl, f)"""
 
-#print(np.array(price_trading["TGT"]["2021-01-28" : "2021-02-12"]))
+#print(np.array(price_trading["TGT"][:10]))
+
